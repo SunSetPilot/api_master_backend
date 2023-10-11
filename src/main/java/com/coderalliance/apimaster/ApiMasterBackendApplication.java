cae8ffc631c5a8b2ef18e49d110bd712d3c543ed
weizhanjun <weizhanjun.sec@outlook.com>
2023-10-11 20:01:44 +0800

    init commit

---
+++ src/main/java/com/coderalliance/apimaster/ApiMasterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.coderalliance.apimaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMasterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMasterBackendApplication.class, args);
+	}
+
+}
