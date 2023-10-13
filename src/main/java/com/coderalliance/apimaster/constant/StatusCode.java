b39b02d4c147d20c33c531e2392794d173cc8de4
whc1202 <1053709303@qq.com>
2023-10-13 18:22:15 +0800

    By Wanghaocheng: add IS_YES and IS_NO

--- src/main/java/com/coderalliance/apimaster/constant/StatusCode.java
+++ src/main/java/com/coderalliance/apimaster/constant/StatusCode.java
@@ -8,7 +8,9 @@ public enum StatusCode {
     ERROR(500),
     BAD_REQUEST(400),
     UNAUTHORIZED(401),
-    FORBIDDEN(403);
+    FORBIDDEN(403),
+    IS_YES(1),
+    IS_NO(0);
 
     private final int code;
 
